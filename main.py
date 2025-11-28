6a7f5aa378151b003392fb06d91ffd3e6e0ecda8
colin <colint.cron@gmail.vcom>
2025-11-28 17:44:19 -0500

    Added a tkinter gui interface for selecting the file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pypdf, click, mimetypes
+import pypdf, mimetypes
 from PIL import Image
+from tkinter import filedialog as fd
 
 def is_pdf(file_path):
     mime_type, _ = mimetypes.guess_type(file_path)
@@ -24,13 +25,24 @@ def jpg_eraser(file):
     img_without_metadata.save(file)
     print("[+] Metadata removed successfully.")
 
-@click.command()
-@click.option('--file', prompt='File name', help='Filename')
 def metadata_eraser(file):
     if is_pdf(file):
         pdf_eraser(file)
     elif is_jpg(file):
         jpg_eraser(file)
 
+def select_file():
+    filetypes = (
+    ('PDF Files', '*.pdf'),
+    ('JPEG Files', '*.jpeg'),
+    ('JPG Files', '*.jpg'),
+    )
+    file = fd.askopenfilename(title='Select file to erase metadata from', filetypes=filetypes)
+    return file
+
+
 if __name__ == "__main__":
-    metadata_eraser()
+    try:
+        metadata_eraser(select_file())
+    except TypeError:
+        print("Program exited")
